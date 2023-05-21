4bcbc63349e5bf3bef284e80b64958726795571c
Habib <habib.alhabib@esprit.tn>
2023-05-21 22:36:17 +0200

    Added get method for region retrieval

--- src/main/java/com/habib/hr/HrApplication.java
+++ src/main/java/com/habib/hr/HrApplication.java
@@ -1,7 +1,16 @@
 package com.habib.hr;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @SpringBootApplication
 public class HrApplication {
@@ -10,4 +19,17 @@ public class HrApplication {
 		SpringApplication.run(HrApplication.class, args);
 	}
 
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+
+	    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+	        for (HttpMessageConverter converter : converters) {
+	            if (converter instanceof org.springframework.http.converter.json.MappingJackson2HttpMessageConverter) {
+	                ObjectMapper mapper = ((MappingJackson2HttpMessageConverter) converter).getObjectMapper();
+	                mapper.registerModule(new Hibernate5Module());
+	                // replace Hibernate4Module() with the proper class for your hibernate version.
+	            }
+	        }
+	    }
+	}
 }
--- src/main/java/com/habib/hr/controllers/RegionController.java
+++ src/main/java/com/habib/hr/controllers/RegionController.java
@@ -2,7 +2,9 @@ package com.habib.hr.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.habib.hr.entities.Region;
@@ -15,8 +17,9 @@ public class RegionController {
 	@Autowired
 	private RegionService service;
 
-	@RequestMapping(path = "/")
-	public ResponseEntity<Region> getRegion(Long id) {
+	@RequestMapping(path = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Region> getRegion(@PathVariable Long id) {
 		Region region = service.getRegion(id);
 		return ResponseEntity.ok(region);
 	}
